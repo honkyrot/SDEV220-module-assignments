04f3afecbde8cdebeefff3e02fff53fa56a40434
honkyrot <hrot3756@gmail.com>
2025-11-27 03:29:50 -0500

    y

---
+++ Module_4_Lab/consume_api.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+response = requests.get(
+    "http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow"
+)
+
+for data in response.json()['items']:
+    if data['answer_count'] > 5:
+        print(data['title'])
+        print(data['link'])
+    else:
+        print("skipped")
+    print()
